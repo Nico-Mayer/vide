8c9da428f2fc26d828524cfaa5b972bf826dd6da
nico-mayer <nico.m94@icloud.com>
2022-06-15 22:46:01 +0200

    logos added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,7 @@
   <main class="flex justify-center items-center w-screen py-20 px-4">
     <div class="flex flex-col items-center w-full">
       <div class="flex flex-col items-center mb-10">
+        <img src="../assets/logo.svg" class="w-60" alt="" />
         <h1 font="semibold" text="2xl">Vide</h1>
         <p opacity="50">Vite superlight Beginner Starter Template</p>
       </div>
